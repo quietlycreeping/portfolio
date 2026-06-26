1b7f0fae69e4543aca0efe2bf60ecedd6d80d61a
quietlycreeping <jorlando@oakland.edu>
2026-06-26 14:31:16 -0400

    building

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,20 @@
+/*=========================================================
+ Author:     J. Orlando
+ Date:       July 2026
+ Description: Homepage with various components
+==========================================================*/
+
+//======Components===========================
+
+const HomePage = () => {
+  return (
+    <>
+    <Header/>
+      <div className="main-content">
+        <p>Home Page here</p>
+      </div>         
+    </>
+  )
+}
+
+export default HomePage
